9618035dddbf07288b22346ac780584423ea3b44
cod3zilla <paranoid.grunge@gmail.com>
2021-08-10 07:08:07 -0700

    i am close to this asshole heroku

--- server/server.js
+++ server/server.js
@@ -1,27 +1,10 @@
 const express=require('express')
-const cors=require('cors')
-if (process.env.NODE_ENV !== 'production') { require('dotenv').config() }
-const connectDB=require('./config/dbconfig')
-const productsRoute=require('./router/productRoute')
-const authRoute=require('./router/authRoute')
-const orderRoute=require('./router/orderRoute')
+
 
 const app=express()
 
-connectDB()
-//middlewares config
-app.use(cors())
-app.use(express.json({urlencoded:true}))
-app.use(express.json())
-//routes config
-app.use('/products',productsRoute)
-app.use('/users',authRoute)
-app.use('/order', orderRoute)
-app.get('/config/paypal', (req, res)=>{
-   res.send(process.env.PAYPAL_CLIENT_ID)
 
-})
-app.get('/home', (req, res)=>{
+app.get('/', (req, res)=>{
   res.send('Hello World..!!!!')
 })
 
